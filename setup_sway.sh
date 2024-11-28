b8db628fbe17d0e82ac4bf773cbfd37190b49c0e
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-11-28 22:47:43 +0900

    Update setup_sway.sh

--- setup_sway.sh
+++ setup_sway.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-sudo pkg install -y drm-kmod libva-intel-driver libva-utils openh264
-sudo pkg install -y xorg-fonts noto-basic noto-jp noto-emoji noto-extra fcitx5 fcitx5-configtool ja-fcitx5-anthy
-sudo pkg install -y firefox
-
-sudo pkg install -y wayland seatd xwayland sway swayidle swaylock-effects waybar foot wofi mako thunar xarchiver pavucontrol alsa-utils networkmgr grim slurp swappy picom
-
-sudo sysrc seatd_enable="YES"
-sudo sysrc kld_list+=i915kms
-" sudo pw groupmod video -m USERNAME
-sudo sysctl net.local.stream.recvspace=65536
-sudo sysctl net.local.stream.sendspace=65536
+sudo pkg install -y sway swayidle swaylock-effects
