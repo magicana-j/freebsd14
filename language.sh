465342863341be3df3162137fd6f4e80d3d98233
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-11-28 22:57:22 +0900

    Create language.sh

---
+++ language.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sleep 2
+echo Install Fonts and IME.
+sleep 2
+sudo pkg install -y xorg-fonts source-code-pro-ttf noto-jp noto-emoji noto-extra fcitx5 fcitx5-configtool ja-fcitx5-anthy
+
+cat << EOF >> ~/.xprofile
+export XMODIFIERS='@im=fcitx'
+export GTK_IM_MODULE=fcitx/xim
+export QT_IM_MODULE=fcitx
+EOF
+
+" mkdir -p ~/.config/autostart && cp /usr/share/applications/org.fcitx.Fcitx5.desktop ~/.config/autostart/
