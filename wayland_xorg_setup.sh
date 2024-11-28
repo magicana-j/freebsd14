35b7271ce252883de0c56a5ba716f85413d76a7d
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-11-28 22:58:21 +0900

    Update wayland_xorg_setup.sh

--- wayland_xorg_setup.sh
+++ wayland_xorg_setup.sh
@@ -2,7 +2,7 @@
 sleep 2
 echo Install Wayland.
 sleep 2
-sudo pkg install -y xorg wayland seatd xwayland alacritty dmenu-wayland dmenu pavucontrol alsa-utils waybar foot wofi mako thunar picom
+sudo pkg install -y xorg wayland seatd xwayland alacritty dmenu-wayland dmenu pavucontrol alsa-utils waybar foot rofi wofi mako thunar picom
 
 sleep 2
 echo Setting SystemControl.
