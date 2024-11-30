b144faffb8a158fb488b56b11765571936edd548
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-11-30 22:13:41 +0900

    Update basic-setup.sh

--- basic-setup.sh
+++ basic-setup.sh
@@ -3,8 +3,6 @@ sudo pkg install -y vim nano neovim htop fastfetch
 sudo pkg install -y drm-kmod libva-intel-driver mesa-libs mesa-dri
 sudo pkg install -y pavucontrol alsa-utils networkmgr
 
-echo Setting SystemControl.
-sleep 2
 sudo sysrc kld_list+=i915kms
 sudo pw groupmod video -m $USER
 sudo sysctl net.local.stream.recvspace=65536
