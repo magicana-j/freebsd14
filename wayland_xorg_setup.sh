e5f7267cb836572407e5844196f242699672e94e
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-11-30 22:11:45 +0900

    Update wayland_xorg_setup.sh

--- wayland_xorg_setup.sh
+++ wayland_xorg_setup.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 sudo pkg install -y xorg wayland seatd xwayland-devel alacritty dmenu-wayland dmenu waybar foot fuzzel wofi mako thunar thunar-archive-plugin picom
 
-sleep 2
-echo Setting SystemControl.
-sleep 2
-sudo sysrc seatd_enable="YES"
-sudo service seatd start
-sudo sysrc dbus_enable="YES"
+echo 'Ececute-- sudo sysrc seatd_enable="YES"'
+echo '  sudo service seatd start'
+echo '  sudo sysrc dbus_enable="YES"'
