3ab90df9b9116b30e9616a1d4fe7eb89d9816be3
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-11-30 22:24:54 +0900

    Update wayland_xorg_setup.sh

--- wayland_xorg_setup.sh
+++ wayland_xorg_setup.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
-sudo pkg install -y xorg wayland seatd xwayland-devel alacritty dmenu-wayland dmenu waybar foot fuzzel wofi mako thunar thunar-archive-plugin picom
+sudo pkg install -y xorg wayland seatd xwayland \
+  alacritty dmenu-wayland waybar foot fuzzel \
+  mako thunar thunar-archive-plugin picom
+
+if [ ! -d "~/.config/alacritty" ]; then
+  mkdir -p ~/.config/alacritty
+fi
+
+if [ ! -d "~/.config/fuzzel" ]; then
+  mkdir -p ~/.config/fuzzel
+fi
+
+if [ ! -d "~/.config/foot" ]; then
+  mkdir -p ~/.config/foot
+fi
+
+if [ ! -d "~/.config/waybar" ]; then
+  mkdir -p ~/.config/waybar
+fi
+
+if [ ! -d "~/.config/mako" ]; then
+  mkdir -p ~/.config/mako
+fi
 
 echo 'Ececute-- sudo sysrc seatd_enable="YES"'
-echo '  sudo service seatd start'
 echo '  sudo sysrc dbus_enable="YES"'
