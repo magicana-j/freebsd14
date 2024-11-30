58483091a2b059e4c4790a1fa67f985c9436f9da
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-11-30 16:52:01 +0900

    Update i3-setup.sh

--- i3-setup.sh
+++ i3-setup.sh
@@ -2,5 +2,5 @@
 sudo pkg install -y i3 alacritty i3status i3blocks i3lock dmenu dunst feh
 mkdir -p ~/.config/i3blocks
 mkdir -p ~/.config/i3status
-cp /usr/local/etc/i3/i3blocks.conf.sample ~/.config/i3blocks/conf
-cp /usr/local/etc/i3/i3status.conf.sample ~/.config/i3status/conf
+cp /usr/local/etc/i3/i3blocks.conf.sample ~/.config/i3blocks/config
+cp /usr/local/etc/i3/i3status.conf.sample ~/.config/i3status/config
