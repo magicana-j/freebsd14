8ca2b6f1f764ec6c2e730555546b4134b092b2b2
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-11-30 22:22:16 +0900

    Update sway-setup.sh

--- sway-setup.sh
+++ sway-setup.sh
@@ -1,9 +1,29 @@
 #!/bin/sh
 
-sudo pkg install -y sway swaybg swayidle waybar swaylock-effects i3status
+sudo pkg install -y sway swaybg swayidle waybar swaylock-effects i3status foot mako
 
 if [ ! -d "~/.config/sway" ]; then
   mkdir -p ~/.config/sway/config.d
+fi
+
+if [ ! -d "~/.config/alacritty" ]; then
+  mkdir -p ~/.config/alacritty
+fi
+
+if [ ! -d "~/.config/foot" ]; then
+  mkdir -p ~/.config/foot
+fi
+
+if [ ! -d "~/.config/waybar" ]; then
+  mkdir -p ~/.config/waybar
+fi
+
+if [ ! -d "~/.config/i3status" ]; then
+  mkdir -p ~/.config/i3status
+fi
+
+if [ ! -d "~/.config/mako" ]; then
+  mkdir -p ~/.config/mako
 fi
 
 cp /usr/local/etc/sway/config ~/.config/sway/
