9c900580e7b2ce6b5f3dceb550b5df50068f5a45
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-05 08:58:27 +0900

    Update wayland_xorg_setup.sh

--- wayland_xorg_setup.sh
+++ wayland_xorg_setup.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 sudo pkg install xorg wayland
-sudo pkg install seatd xwayland \
+sudo pkg install seatd xwayland ly \
   alacritty dmenu-wayland waybar foot fuzzel \
   mako thunar thunar-archive-plugin nemo nemo-fileroller picom
 
 echo 'Ececute-- sudo sysrc seatd_enable="YES"'
 echo '  sudo sysrc dbus_enable="YES"'
+echo '  sudo sysrc ly_enable="YES"
