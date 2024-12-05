5fefe59d2e49378158bfedd89a4a867e7438b384
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-05 20:36:02 +0900

    Update wayland_xorg_setup.sh

--- wayland_xorg_setup.sh
+++ wayland_xorg_setup.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 sudo pkg install xorg wayland
-sudo pkg install seatd xwayland ly \
-  alacritty dmenu-wayland waybar foot fuzzel \
-  mako thunar thunar-archive-plugin nemo nemo-fileroller picom
-
+sudo pkg install seatd xwayland
 sudo sysrc seatd_enable="YES"
 sudo sysrc dbus_enable="YES"
-sudo sysrc ly_enable="YES"
 
 sudo echo 'proc /proc procfs rw 0 0' >> /etc/fstab
