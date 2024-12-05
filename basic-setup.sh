c76b077efb7647b960a5185dc641300862615962
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-05 20:38:07 +0900

    Update basic-setup.sh

--- basic-setup.sh
+++ basic-setup.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-sudo pkg install -y vim nano neovim htop fastfetch
-sudo pkg install -y drm-kmod libva-intel-driver mesa-libs mesa-dri
-sudo pkg install -y pavucontrol alsa-utils networkmgr
+sudo pkg install -y vim nano neovim htop gtop fastfetch
+sudo pkg install -y drm-kmod libva-intel-driver mesa-libs mesa-dri libva-utils gpu-firmware-intel-kmod-kabylake
+sudo pkg install -y pipewire wireplumber pavucontrol alsa-utils networkmgr
 
 sudo sysrc kld_list+=i915kms
+sudo sysrc kld_list+=linux
+sudo sysrc kld_list+=linux64
+
 sudo pw groupmod video -m $USER
+sudo pw groupmod audio -m $USER
+
 sudo sysctl net.local.stream.recvspace=65536
 sudo sysctl net.local.stream.sendspace=65536
