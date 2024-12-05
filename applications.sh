759e4585a08d1ce415793a08c7bbcbea256e9275
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-05 20:42:57 +0900

    Update applications.sh

--- applications.sh
+++ applications.sh
@@ -2,4 +2,7 @@
 sleep 2
 echo Install Applications.
 sleep 2
-sudo pkg install -y firefox cava simplescreenrecorder handbrake xdg-user-dirs openh264 zip unzip xarchiver networkmgr automount v4l-utils v4l_compat
+sudo pkg install -y firefox cava simplescreenrecorder \
+  handbrake xdg-user-dirs openh264 zip unzip xarchiver \
+  networkmgr automount v4l-utils v4l_compat \
+  vlc obs-studio shotcut shotwell
