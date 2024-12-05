2db9cef776a47c255d115b8e6d0f387217418e60
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-05 20:39:36 +0900

    Update applications.sh

--- applications.sh
+++ applications.sh
@@ -2,11 +2,4 @@
 sleep 2
 echo Install Applications.
 sleep 2
-sudo pkg install -y firefox cava simplescreenrecorder handbrake libva-intel-driver libva-utils xdg-user-dirs openh264 zip unzip xarchiver neofetch fastfetch networkmgr automount v4l-utils v4l_compat
-
-sleep 2
-echo Install WebCam Applications.
-sleep 2
-sudo pkg install -y webcamd
-sudo sysrc webcamd_enable="YES"
-sudo pw groupmod webcamd -m $USER
+sudo pkg install -y firefox cava simplescreenrecorder handbrake xdg-user-dirs openh264 zip unzip xarchiver networkmgr automount v4l-utils v4l_compat
