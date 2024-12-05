4d82c93a6d1c62137a99096a295e20bda6e769a7
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-05 20:50:38 +0900

    Update sway-setup-freebsd.sh

--- sway-setup-freebsd.sh
+++ sway-setup-freebsd.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-sudo pkg install wayland xwayland seatd xdg-desktop-portal-gtk \
+sudo pkg xdg-desktop-portal-gtk xfce4-terminal \
   fuzzel foot alacritty nwg-drawer nwg-launchers \
   waybar swaybg swaylock waylock swayidle \
   wf-recorder wl-clipboard wob wlogout xbrightness \
   gvfs fusefs-ntfs \
   xarchiver nemo mako volumeicon kanshi \
-  sway
+  sway cinnamon lightdm lightdm-gtk-greeter
+
+sudo sysrc lightdm_enable="YES"
