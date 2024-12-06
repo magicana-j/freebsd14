7e79efa595d4965678a6d4e7cd27a396a993c795
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-06 21:47:22 +0900

    Update and rename desktop-setup.sh to sway-setup.sh

--- sway-setup.sh
+++ sway-setup.sh
@@ -6,9 +6,4 @@ sudo pkg xdg-desktop-portal-gtk xfce4-terminal \
   wf-recorder wl-clipboard wob wlogout xbrightness \
   gvfs fusefs-ntfs \
   xarchiver nemo mako volumeicon kanshi \
-  sway cinnamon sddm
-
-sudo sysrc sddm_enable="YES"
-sudo sysrc sddm_lang="en_GB"
-
-echo 'setxkbmap -layout jp' >> ~/.xinitrc
+  sway
