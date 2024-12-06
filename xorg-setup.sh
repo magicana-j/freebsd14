96b716e93a40c88742264bd8fee518414c729370
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-06 22:37:10 +0900

    xorg-setup.sh を更新

--- xorg-setup.sh
+++ xorg-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-sudo pkg install xorg
+sudo pkg install xorg sddm
 sudo sysrc dbus_enable="YES"
+sudo sysrc sddm_enable="YES"
 sudo echo 'proc /proc procfs rw 0 0' >> /etc/fstab
 echo 'setxkbmap -layout jp' >> ~/.xinitrc
