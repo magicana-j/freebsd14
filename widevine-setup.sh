72cbd6afccf4cf1bced09f4235cee8571fc204c0
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-07 09:09:43 +0900

    Update widevine-setup.sh

--- widevine-setup.sh
+++ widevine-setup.sh
@@ -7,7 +7,12 @@ sudo pkg install foreign-cdm
 sudo sysrc linux_enable="YES"
 sudo service linux start
 
-git clone --depth 1 https://github.com/freebsd/freebsd-ports
-cd freebsd-ports/www/linux-widevine-cdm
-make
-sudo make install
+if [ ! -d /usr/ports/www ]; then
+  git clone --depth 1 https://github.com/freebsd/freebsd-ports
+  cd freebsd-ports/www/linux-widevine-cdm
+  make
+  sudo make install
+else
+  cd /usr/ports/www/chromium
+  sudo make install clean
+fi
