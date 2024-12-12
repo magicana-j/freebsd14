055aef6aea63848847a5a24baf133b4ddf397deb
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-12 22:08:10 +0900

    applications.sh を更新

--- applications.sh
+++ applications.sh
@@ -2,7 +2,10 @@
 sleep 2
 echo Install Applications.
 sleep 2
-sudo pkg install -y firefox simplescreenrecorder \
-  handbrake xdg-user-dirs x264 openh264 zip unzip xarchiver \
-  automount v4l-utils v4l_compat \
-  vlc obs-studio shotcut shotwell
+sudo pkg install -y firefox
+sudo pkg install -y simplescreenrecorder \
+  handbrake xdg-user-dirs x264 openh264
+sudo pkg install -y zip unzip xarchiver \
+  automount v4l-utils v4l_compat
+sudo pkg install -y vlc obs-studio shotcut shotwell \
+  gnome-keyring vscode
