304290d8a279a8d26f709775c73edec69460d6c4
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-13 09:37:42 +0900

    Update language.sh

--- language.sh
+++ language.sh
@@ -8,13 +8,14 @@ sudo pkg install -y noto-jp
 sudo pkg install -y fcitx5 fcitx5-configtool fcitx5-gtk2 fcitx5-gtk3 fcitx5-gtk4 fcitx5-qt5 fcitx5-qt6 ja-fcitx5-anthy
 fc-cache -fv
 
-cat << EOF >> ~/.xinitrc
+cat << EOF >> ~/.profile
 export LC_COLLATE="ja_JP.utf-8"
 export LC_CTYPE="ja_JP.utf-8"
 export LC_MONETARY="ja_JP.utf-8"
 export LC_NUMERIC="ja_JP.utf-8"
 export LC_TIME="ja_JP.utf-8"
 
+cat << EOF >> ~/.xinitrc
 export XMODIFIERS='@im=fcitx'
 export GTK_IM_MODULE=fcitx/xim
 export QT_IM_MODULE=fcitx
