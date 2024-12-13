55fe7fe9a408c2982fea4fbbaa4041c8994e8a43
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-13 10:12:22 +0900

    Update zsh-configuration.sh

--- zsh-configuration.sh
+++ zsh-configuration.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
 cat << EOF >> ~/.zshrc
+setopt print_eight_bit
+setopt auto_cd
+setopt auto_pushd
+
+export HISTSIZE=1000
+export SAVEHIST=1000
+setopt share_history
+setopt hist_reduce_blanks
+setopt hist_ignore_all_dups
+
 alias ls='ls --color=auto'
 alias la='ls -a'
 alias ll='ls -al | less'
